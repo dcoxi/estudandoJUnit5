8be3683f2a375c556b7a460056b957d193cef045
dcoxi <d_pc1@hotmail.com>
2022-11-18 16:31:14 +0100

    junit 5 anotação

---
+++ src/test/java/anotation/TestAnnotationDemo.java
@@ -0,0 +1,14 @@
+package anotation;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAnnotationDemo {
+
+    @Test
+    void test(){
+       assertEquals(2,2);
+    }
+}
