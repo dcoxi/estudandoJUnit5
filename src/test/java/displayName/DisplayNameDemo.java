7da3a75ad6efbcb242f0d3513e2ccd3d78baa2bc
dcoxi <d_pc1@hotmail.com>
2022-11-22 15:57:43 +0100

    Anotação @DisplayName

---
+++ src/test/java/displayName/DisplayNameDemo.java
@@ -0,0 +1,19 @@
+package displayName;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("Display Name Demo")
+class DisplayNameDemo {
+
+
+    @Test
+    @DisplayName("Display Name Demo Test")
+    void displayNameDemoTest(){
+        assertEquals(3,2+1, "Os valores são diferentes");
+    }
+
+}
