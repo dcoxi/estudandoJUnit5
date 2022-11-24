514463f10ef88c132c1297cfc5a50c2d17d1d414
dcoxi <d_pc1@hotmail.com>
2022-11-24 09:34:45 +0100

    AssumThat about

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,22 @@
 package main.java;
+import java.util.Collections;
+import  java.util.List;
+import java.util.ArrayList;
+
 
 public class Application {
 
     public static void main(String[] args){
+
+        List<String> listString = new ArrayList<>();
+        listString.add("1");
+        listString.add("2");
+        listString.add("3");
+        listString.add("4");
+        listString.add("5");
+
+        Collections.sort(listString, Collections.reverseOrder());
+
        System.out.println("Estrutura projecto");
     }
 
